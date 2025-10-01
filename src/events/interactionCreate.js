b531d44fb4fa3f3534c89ad6e826c1277accea71
devnawa <148815756+devnawa@users.noreply.github.com>
2025-10-01 19:38:23 +0300

    feat: add button handler

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -2,7 +2,6 @@ import { Events } from "discord.js"
 import { readdirSync } from "fs";
 
 const applicationCommands = new Map()
-
 const commandFiles = readdirSync("./src/commands/applicationCommands").filter(file => file.endsWith(".js"));
 console.log(`Found ${commandFiles.length} command files. (Interaction create)`);
 for (const file of commandFiles) {
@@ -19,6 +18,23 @@ for (const file of commandFiles) {
     }
 }
 
+const buttons = new Map()
+const buttonFiles = readdirSync("./src/commands/buttons").filter(file => file.endsWith(".js"));
+console.log(`Found ${buttonFiles.length} button command files. (Interaction create)`);
+for (const file of buttonFiles) {
+    try {
+        const buttonCommand = await import(`../commands/buttons/${file}`);
+        if (buttonCommand && buttonCommand.default && buttonCommand.default.customId && buttonCommand.default.execute) {
+            buttons.set(buttonCommand.default.customId, buttonCommand.default);
+            console.log(`Loaded button command: ${buttonCommand.default.customId}`);
+        } else {
+            console.warn(`Invalid button command file: ${file}`);
+        }
+    } catch (err) {
+        console.error(`Error loading button command file ${file}:`, err);
+    }
+}
+
 export default async client => {
 
     client.on(Events.InteractionCreate, async interaction => {
@@ -26,8 +42,11 @@ export default async client => {
         let command
 
         if (interaction.isChatInputCommand?.()) {
-            console.log(interaction.commandName)
             command = applicationCommands.get(interaction.commandName);
+        }
+
+        if (interaction.isButton?.()) {
+            command = buttons.get(interaction.customId);
         }
 
         if (!command) {
